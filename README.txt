69d20ad666b120db5687671f54be98dd79b1d45b
Taylor Jordan <Taylor.Jordan@bcg.com>
2016-04-07 15:17:34 -0400

    Committed

---
+++ README.txt
@@ -0,0 +1,11 @@
+DM panel created by Jordan Taylor
+
+The DM panel allows dungeon masters to play atmospheric music and take notes during their RPGs
+
+Notes on code below::
+
+	Used express to create the skeleton for this project
+
+	Main code logic is in public/javascripts/global.js (See there for a general description of the project)
+	
+	Main UI code is in views/panelhomepage.jade (coded in Jade)
--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -2,28 +2,52 @@
 //object to hold values of needed functions and variables so we don't pollute the namespace
 var dmpanelfunctionsandvalues = {};
 
-//DOM Ready
+/*
+DOM READY EVENT: READ HERE
+
+When home page loads, we refer to the musicfiles object contained in this file
+
+musicfiles is a JS object where each key is text referring to a kind of music
+you might want to play (ex "Walking through forest") and the value for that is the path to a
+music file appropriate for that mood (e.g. "Zelda Kokiri Forest track")
+
+Upon dom loading, the function below refers to the musicfiles object, and creates a new button for 
+each key value pair in the object. Each button is labeled with the mood you want to create and plays
+the corresponding song
+
+Organizing things this way allows easy update functionality: just add a new mood and song
+
+Currently only songs loaded include a good sustained activity song ("Fathersandsons") and an epic emotional climax song ("Lux Aeterna")
+
+Adding others in future updates
+
+*/
 $(document).ready(function() {
 	
 	each(dmpanelfunctionsandvalues.musicfiles,(function(key){
+		
 		var newbutton = document.createElement("button");
+		
 		newbutton.value = key;
+		
 		newbutton.id = key;
+		
 		newbutton.appendChild(document.createTextNode(key))
+		
 		var buttoncontainer = document.getElementById("button-container");
+		
 		buttoncontainer.appendChild(newbutton);
+		
 		document.getElementById(key).onclick = dmpanelfunctionsandvalues.playmusic(dmpanelfunctionsandvalues.musicfiles[key]);
+		
 		buttoncontainer.appendChild(document.createElement("br"));
+		
 		buttoncontainer.appendChild(document.createElement("br"));
 		})
 	);
 
 });
 
-//Alternate implementation: could create all buttons agnostically in jade, then rename all the buttons and assign music here
-//so have all button names live here and use the script to create them, do that, could also take a higher order function
-//and you can use map to create the buttons
-//and that way just adding to this list of buttons will add to it
 
 
 //** FUNCTIONS AND VARIABLES
@@ -41,7 +65,7 @@ function each(collection,func){
 	}
 }
 
-//object holding names of tracks and tracks; new button will be created for each key value pair
+//object holding names of tracks and tracks; see readme at top
 dmpanelfunctionsandvalues.musicfiles = {
 	"Sustained music for cheery progress or ongoing activity":"fathersandsons.mp3",
 	"Sustained music for eerie investigation":"fathersandsons.mp3",
