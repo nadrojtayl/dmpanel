7490474aee03a587ba81dc67aa6f8d6a26b31469
Taylor Jordan <Taylor.Jordan@bcg.com>
2016-04-08 16:28:47 -0400

    Message

--- README.txt
+++ README.txt
@@ -1,11 +1,13 @@
 DM panel created by Jordan Taylor
 
-The DM panel allows dungeon masters to play atmospheric music and take notes during their RPGs
+The DM panel allows dungeon masters to play atmospheric music, takes notes, and create NPCs on the fly while DMing
+
+
 
 Notes on code below::
 
 	Used express to create the skeleton for this project
 
-	Main code logic is in public/javascripts/global.js (See there for a general description of the project)
+	Main code logic is in public/javascripts/global.js (See there for a general description of the project, as well as functions that handle music and character creation)
 	
 	Main UI code is in views/panelhomepage.jade (coded in Jade)
--- public/javascripts/charactercreator.js
+++ public/javascripts/charactercreator.js
@@ -1,156 +1,10 @@
-
-//object to hold values of needed functions and variables so we don't pollute the namespace
-var dmpanelfunctionsandvalues = {};
-
-/*
-DOM READY EVENT: READ HERE
-
-When home page loads, we refer to the musicfiles object contained in this file
-
-musicfiles is a JS object where each key is text referring to a kind of music
-you might want to play (ex "Walking through forest") and the value for that is the path to a
-music file appropriate for that mood (e.g. "Zelda Kokiri Forest track")
-
-Upon dom loading, the function below refers to the musicfiles object, and creates a new button for 
-each key value pair in the object. Each button is labeled with the mood you want to create and plays
-the corresponding song
-
-Organizing things this way allows easy update functionality: just add a new mood and song
-
-Currently only songs loaded include a good sustained activity song ("Fathersandsons") and an epic emotional climax song ("Lux Aeterna")
-
-Adding others in future updates
-
-*/
-$(document).ready(function() {
-	//upon page load, populates document with buttons to play music
-	each(dmpanelfunctionsandvalues.musicfiles,(function(key){
-		
-		var newbutton = document.createElement("button");
-		
-		newbutton.value = key;
-		
-		newbutton.id = key;
-		
-		newbutton.appendChild(document.createTextNode(key))
-		
-		var buttoncontainer = document.getElementById("button-container");
-		
-		buttoncontainer.appendChild(newbutton);
-		
-		document.getElementById(key).onclick = dmpanelfunctionsandvalues.playmusic(dmpanelfunctionsandvalues.musicfiles[key]);
-		
-		buttoncontainer.appendChild(document.createElement("br"));
-		
-		buttoncontainer.appendChild(document.createElement("br"));
-		})
-	);
-
-});
-
-
-
-//** FUNCTIONS AND VARIABLES
-
-
-//helper function each
-	function each(collection,func){
-		if (Array.isArray(collection)){
-			for (var i =0;i<collection.length;i++){
-					func(collection[i]);
-			}
-		}
-		for (var i in collection){
-			func(i);
-		}
-	}
-
-//object holding names of tracks and tracks; see readme at top
-	dmpanelfunctionsandvalues.musicfiles = {
-		"Sustained music for cheery progress or ongoing activity":"fathersandsons.mp3",
-		"Sustained music for eerie investigation":"fireworkshungergames.mp3",
-		"Music for sudden danger":"trinitydream.mp3",
-		"Music for emotional payoff":"luxaeterna.mp3",
-		"Tavern music":"ConanTheology.mp3"
-	}
-
-//plays music of filename attached to button
-	dmpanelfunctionsandvalues.playmusic = function(filename){
-		return function(){
-			var songfile = new Audio(filename);
-			songfile.play();
-			console.log("Now playing " + filename);
-		}
-	}
-
-//creates an NPC based on user input and rules of DND 3.5
-//outputs NPC stats to block on page
-//NOTE: Utilizes several functions comprising a DND stat calculator, all at bottom of this page
-
-	function makecharacter(){
-		//gets character inputs from user,defining level,class,name,etc
-		var level = document.getElementById("level").value;
-		var charclass = document.getElementById("charclass").value;
-		var name = document.getElementById("charname").value;
-		if (!name) {var name = "Nameless, the Mysterious";}
-		var equipmentquality=document.getElementById("equipment").value;
-		
-		//uses inputs to calculate character statblock (see functions that handle calculations below)
-		//for now assumes all ability scores are 16
-		var charstats = createcharacter(name.toString(),charclass,level,{Str:16,Dex:16,Con:16,Int:16,Wis:16,Cha:16},equipmentquality);
-
-		//helper function that turns an object into a string representing its contents: allowing us to print stat block to screen
-		function printobject(myobject){
-		var toreturn = "";
-		for (var i in myobject){
-			toreturn += i;
-			toreturn += " : "
-			toreturn += myobject[i];
-			toreturn += " ";
-			toreturn += "\n"
-		}
-		return toreturn;
-	}
-		
-		//appends character stat block to DOM
-		var newtextarea = document.createElement("textarea");
-		newtextarea.value = printobject(charstats);
-		document.getElementById("charactertext").appendChild(newtextarea);
-		
-	}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-
-//FUNCTIONS THAT HANDLE DND CHARACTER STATS CREATION
-
-
-
-*/
-
 /*Goal: Generates a stat block for a DND character (e.g. hp, melee attack, saves, initiative, AC, skill points)
 based on inputted name,character class,character level, and attributes
 */
 
+//load prompt module
+
+	//var prompt = require('prompt');
 
 //generate ability score modifier
 	function abilityscoremodcalc(score){
@@ -193,17 +47,17 @@ based on inputted name,character class,character level, and attributes
 		
 		//object defining quality level of each stat for each class 
 		var classqualities = {
-			Bard: {HP:'Mediocre',Skillpoints:'Mediocre',BAB:'Medium',Fort:'Bad',Ref:'Good',Will:'Good'},
-			Barbarian:{HP:'Great',Skillpoints:'Bad',BAB:'Good',Fort:'Good',Ref:'Good',Will:'Bad'},
-			Cleric:{HP:'Okay',Skillpoints:'Bad',BAB:'Medium',Fort:'Good',Ref:'Bad',Will:'Good'},
-			Druid:{HP:'Okay',Skillpoints:'Bad',BAB:'Medium',Fort:'Good',Ref:'Bad',Will:'Good'},
-			Rogue: {HP:'Okay',Skillpoints:'Okay',BAB:'Medium',Fort:'Bad',Ref:'Good',Will:'Bad'},
-			Ranger:{HP:'Okay',Skillpoints:'Mediocre',BAB:'Good',Fort:'Good',Ref:'Good',Will:'Bad'},
-			Wizard:{HP:'Bad',Skillpoints:'Mediocre',BAB:'Good',Fort:'Good',Ref:'Good',Will:'Bad'},
-			Sorcerer:{HP:'Okay',Skillpoints:'Bad',BAB:'Bad',Fort:'Bad',Ref:'Bad',Will:'Good'},
-			Fighter:{HP:'Good',Skillpoints:'Bad',BAB:'Good',Fort:'Good',Ref:'Bad',Will:'Bad'},
-			Monk:{HP:'Okay',Skillpoints:'Mediocre',BAB:'Medium',Fort:'Good',Ref:'Good',Will:'Good'},
-			Paladin:{HP:'Good',Skillpoints:'Bad',BAB:'Good',Fort:'Good',Ref:'Bad',Will:'Bad'}
+			Bard: {HP:'Mediocre',Skillpoints:'Mediocre',BAB:'Medium',Fort:'Bad',Ref:'Good',Will:'Good'}
+			/*Barbarian:{HP:Great,Skillpoints:Bad,BAB:Good,}
+			Cleric:{HP:}
+			Druid:{}
+			Rogue:{}
+			Ranger:{}
+			Wizard:{}
+			Sorcerer:{}
+			Fighter:{}
+			Monk:{}
+			Paladin:{}	*/
 		}
 		
 		//function that calculates a characters stat if the stat is gradated (like HP)
@@ -214,6 +68,7 @@ based on inputted name,character class,character level, and attributes
 		//function that calculates a characters stat if the stat is not gradated (like Skills)
 		function calculategradatedskill(statname,charclass,level){
 			if (['Fort','Ref','Will'].indexOf(statname) !== -1) {savename =statname; statname =  "Saves";} else {savename = statname;	}
+			console.log(statname); console.log(baseattributeclasslevellookup[statname][classqualities[charclass][savename]])
 			return baseattributeclasslevellookup[statname][classqualities[charclass][savename]](level);
 		}
 		
@@ -284,6 +139,7 @@ based on inputted name,character class,character level, and attributes
 //adds equipment effects to character stats, based on inputted descriptor of equipment quality
 	function addequipment(equipmentquality,statswithoutequipment){
 		var statswithequipment = statswithoutequipment;
+		
 		if (equipmentquality==="Good"){
 			statswithequipment.AC += 4;
 			statswithequipment.RangedWeapon = "Longbow";
@@ -302,13 +158,20 @@ based on inputted name,character class,character level, and attributes
 	
 //returns character based on inputted class, level, attributes and equipment quality
 	function createcharacter(name,charclass,level,attributes,equipmentquality){
+		
 		var characterbasestats = characterbasecalculator(charclass,level);
 		
 		var statsmodifiedbasedonattributes = addatrributeeffects(characterbasestats,attributes);
 		
 		var finalstats = addequipment(equipmentquality,statsmodifiedbasedonattributes)
 		
-		finalstats.Name = name;
+		finalstats.name = name;
 		
 		return finalstats;
 	}
+
+	
+//// TESTS (WILL EXPAND THIS)
+	var jake = characterbasecalculator('Bard',14);
+	console.log(createcharacter('Jake','Bard',14,{Str:16,Dex:26,Con:14,Int:12,Wis:10,Cha:11},"Good"));
+	console.log(addatrributeeffects(jake,{Str:16,Dex:26,Con:14,Int:12,Wis:10,Cha:11}));
